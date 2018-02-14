bbdd5977aee338e5d00dee35cddef731e55ce8ba
中本昌吾 <shogonakamoto0109@gmail.com>
2018-02-14 21:45:55 +0900

    .gitignore is now working

--- questions/config.py
+++
@@ -1,9 +0,0 @@
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///questions.db'
-SECRET_KEY = 'secret key'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-UPLOAD_FOLDER = 'upload/'
-# 分岐の数
-BRANCH_NUMBER = 1
-# 質問（保存する回答）の数
-QUESTION_NUMBER = 30
